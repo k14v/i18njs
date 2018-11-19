9b29d816aac1453668beb89e3ef6f00326b5e7f5
rubeniskov <me@rubeniskov.com>
2018-11-19 22:10:51 +0100

    docs: added basic example

---
+++ examples/basic.js
@@ -0,0 +1,23 @@
+import i18njs from '..';
+
+const i18n = i18njs({
+  locales: ['en', 'de'],
+  resolver: (locale) => ({
+    'esto es una prueba': locale === 'en' ? 'this is a test' : 'Das ist ein Test'
+  })
+})
+
+
+
+i18n.onReady(() => {
+
+    console.log(i18n.in.__('esto es una prueba'))
+
+    i18n.setLocale('en')
+
+    console.log(i18n.in.__('esto es una prueba'));
+
+    i18n.setLocale('de')
+
+    console.log(i18n.in.__('esto es una prueba'));
+})
