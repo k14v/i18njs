e51e9b5da6f56c7c0c7c0369aae6c81f5a278833
rubeniskov <me@rubeniskov.com>
2018-11-19 23:07:04 +0100

    feat: add checker catalog definition when call interpolators

--- examples/basic.js
+++ examples/basic.js
@@ -1,20 +1,22 @@
-import i18njs from '..'
+import i18njs from '..';
 
 const i18n = i18njs({
   locales: ['en', 'de'],
   resolver: (locale) => ({
     'esto es una prueba': locale === 'en' ? 'this is a test' : 'Das ist ein Test'
   })
-})
+});
+
+console.log(i18n.in.__('esto es una prueba'));
 
 i18n.onReady(() => {
-  console.log(i18n.in.__('esto es una prueba'))
+  console.log(i18n.in.__('esto es una prueba'));
 
-  i18n.setLocale('en')
+  i18n.setLocale('en');
 
-  console.log(i18n.in.__('esto es una prueba'))
+  console.log(i18n.in.__('esto es una prueba'));
 
-  i18n.setLocale('de')
+  i18n.setLocale('de');
 
-  console.log(i18n.in.__('esto es una prueba'))
-})
+  console.log(i18n.in.__('esto es una prueba'));
+});
--- src/createInterpolators.js
+++ src/createInterpolators.js
@@ -1,9 +1,16 @@
-export default (catalog = {}) => {
-  const process = (literal) => literal
+const notYetImplemented = (catalog) => fn => (...args) => {
+  if (!catalog) {
+    console.warn('No tieneee catalogoooo');
+  }
+  return fn(...args);
+};
+
+export default (catalog) => {
+  const process = (literal) => literal;
 
   return {
-    __: (literal) => {
-      return process(catalog[literal] || literal)
-    }
-  }
-}
+    __: notYetImplemented(catalog)(literal => {
+      return process((catalog && catalog[literal]) || literal);
+    })
+  };
+};
