3074dcba699020faac8e5a9648c6fa0162611d1d
rubeniskov <me@rubeniskov.com>
2018-11-19 23:53:14 +0100

    feat: add log traces when call interpolators with no catalog or literal defined

--- src/createInterpolators.js
+++ src/createInterpolators.js
@@ -1,16 +1,32 @@
-const notYetImplemented = (catalog) => fn => (...args) => {
-  if (!catalog) {
-    console.warn('No tieneee catalogoooo');
+const catalogCheckerDecorator = (catalog) => fn => (...args) => {
+  if (!catalog && process.env.NODE_ENV !== 'production') {
+    console.warn(`// WARNING: Not catalog defined yet when calling interpolator with arguments: ${args}`);
   }
+
   return fn(...args);
+};
+
+const getterLiteral = catalog => literal => {
+  if (catalog && catalog[literal]) {
+    return catalog[literal];
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn(`// WARNING: Missing literal translation "${literal}" in catalog, using it as fallback`);
+  }
+
+  return literal;
 };
 
 export default (catalog) => {
   const process = (literal) => literal;
-
-  return {
-    __: notYetImplemented(catalog)(literal => {
-      return process((catalog && catalog[literal]) || literal);
-    })
+  const getLiteral = getterLiteral(catalog);
+  const interpolators = {
+    __: (literal) => process(getLiteral(literal))
   };
+
+  return Object.keys(interpolators).reduce((prev, inName) => ({
+    ...prev,
+    [inName]: catalogCheckerDecorator(catalog)(interpolators[inName]) }),
+  {});
 };
