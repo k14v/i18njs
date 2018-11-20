bd82e4683e9150cdbf94ae5176fec7fe88cd33da
Kelvur <Osorio10.92@gmail.com>
2018-11-21 00:09:28 +0100

    feat: improved examples, now with more locales!

--- examples/basic.js
+++ examples/basic.js
@@ -2,25 +2,37 @@ import i18njs from '..';
 
 const i18n = i18njs({
   locales: {
+    es: {
+      'esto es una prueba': 'esto es una prueba',
+      'Teléfono': 'Teléfono',
+      'Perro': 'Perro',
+    },
     en: {
-      'esto es una prueba': 'this is a test'
+      'esto es una prueba': 'this is a test',
+      'Teléfono': 'Phone',
+      'Perro': 'Dog',
     },
     de: {
-      'esto es una prueba': 'Das ist ein Test'
-    }
-  }
+      'esto es una prueba': 'Das ist ein Test',
+      'Teléfono': 'Telefon',
+      'Perro': 'Hund',
+    },
+  },
 });
 
 console.log(i18n.trls.__('esto es una prueba'));
 
 i18n.onReady(() => {
   console.log(i18n.trls.__('esto es una prueba'));
+  console.log(i18n.trls.__('Teléfono'));
 
   i18n.setLocale('en');
 
   console.log(i18n.trls.__('esto es una prueba'));
+  console.log(i18n.trls.__('Teléfono'));
 
   i18n.setLocale('de');
 
   console.log(i18n.trls.__('esto es una prueba'));
+  console.log(i18n.trls.__('Teléfono'));
 });
--- examples/resolver.js
+++ examples/resolver.js
@@ -1,22 +1,40 @@
 import i18njs from '..';
 
+const locales = {
+  es: {
+    'esto es una prueba': 'esto es una prueba',
+    'Teléfono': 'Teléfono',
+    'Perro': 'Perro',
+  },
+  en: {
+    'esto es una prueba': 'this is a test',
+    'Teléfono': 'Phone',
+    'Perro': 'Dog',
+  },
+  de: {
+    'esto es una prueba': 'Das ist ein Test',
+    'Teléfono': 'Telefon',
+    'Perro': 'Hund',
+  },
+};
+
 const i18n = i18njs({
-  locales: ['en', 'de'],
-  resolver: (locale) => ({
-    'esto es una prueba': locale === 'en' ? 'this is a test' : 'Das ist ein Test'
-  })
+  locales: ['es', 'en', 'de'],
+  locale: 'es',
+  resolver: (locale) => locales[locale],
 });
 
 console.log(i18n.trls.__('esto es una prueba'));
 
 i18n.onReady(() => {
   console.log(i18n.trls.__('esto es una prueba'));
+  console.log(i18n.trls.__('Teléfono'));
 
   i18n.setLocale('en');
 
-  console.log(i18n.trls.__('esto es una prueba'));
+  console.log(i18n.trls.__('Teléfono'));
 
   i18n.setLocale('de');
 
-  console.log(i18n.trls.__('esto es una prueba'));
+  console.log(i18n.trls.__('Perro'));
 });
