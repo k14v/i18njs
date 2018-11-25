285c010f079425a6c891423a15488c23f031c9ad
Kelvur <kat_mig_oso@hotmail.com>
2018-11-25 22:52:27 +0100

    feat: add __n function, now you can translate singular and plural literals

--- examples/resolver.js
+++ examples/resolver.js
@@ -6,16 +6,28 @@ const locales = {
     'esto es una prueba': 'esto es una prueba',
     'Teléfono': 'Teléfono',
     'Perro': 'Perro',
+    'Tengo %s gato': {
+      'one': 'Tengo un gato',
+      'other': 'Tengo algunos gatos',
+    },
   },
   en: {
     'esto es una prueba': 'this is a test',
     'Teléfono': 'Phone',
     'Perro': 'Dog',
+    'Tengo %s gato': {
+      'one': 'I have a cat',
+      'other': 'I have some cats',
+    },
   },
   de: {
     'esto es una prueba': 'Das ist ein Test',
     'Teléfono': 'Telefon',
     'Perro': 'Hund',
+    'Tengo %s gato': {
+      'one': 'Ich habe eine katze',
+      'other': 'Ich habe mehrere Katzen',
+    },
   },
 };
 
@@ -46,12 +58,14 @@ setTimeout(() => {
   i18n.setLocale('en').then(trls => {
     console.log(trls.__('esto es una prueba'));
     console.log(trls.__('Teléfono'));
+    console.log(trls.__n('Tengo %s gato', 1));
+    console.log(trls.__n('Tengo %s gato', 5));
   });
-}, 2000);
+});
 
-setTimeout(() => {
-  i18n.setLocale('de').then(trls => {
-    console.log(i18n.trls.__('esto es una prueba'));
-    console.log(i18n.trls.__('Teléfono'));
-  });
-}, 4000);
+// setTimeout(() => {
+//   i18n.setLocale('de').then(trls => {
+//     console.log(i18n.trls.__('esto es una prueba'));
+//     console.log(i18n.trls.__('Teléfono'));
+//   });
+// }, 4000);
--- src/createTranslators.js
+++ src/createTranslators.js
@@ -13,18 +13,36 @@ const getterLiteral = catalog => literal => {
   if (catalog && catalog[literal]) {
     return catalog[literal];
   }
+};
 
-  warn(`Missing literal translation "${literal}" in catalog, using it as fallback`);
+const getterLiteralPlural = catalog => (literal, count) => {
+  if (catalog) {
+    if (typeof catalog[literal] === 'object') {
+      return count === 1 ? catalog[literal].one : catalog[literal].other;
+    }
+    return catalog[literal];
+  }
+};
 
-  return literal;
+const process = (literal, fallback) => {
+  if (!literal) {
+    warn(`Missing literal translation "${fallback}" in catalog, using it as fallback`);
+    return fallback;
+  }
+  return postProcessLiteral(literal);
+};
+
+const postProcessLiteral = literal => {
+  return typeof literal === 'object' ? literal.one : literal;
 };
 
 export default (catalog) => {
-  const process = (literal) => literal;
   const catalogChecker = createCatalogChecker(catalog);
   const getLiteral = getterLiteral(catalog);
+  const getLiteralPlural = getterLiteralPlural(catalog);
   const translators = {
-    __: (literal) => process(getLiteral(literal)),
+    __: (literal) => process(getLiteral(literal), literal),
+    __n: (literal, count) => process(getLiteralPlural(literal, count), literal),
   };
 
   return Object.keys(translators).reduce((prev, trlsName) => ({
